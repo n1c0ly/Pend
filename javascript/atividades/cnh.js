bd395c12175518a72611c8665377fff44d9981fd
n1c0ly <santosnico048@gmail.com>
2025-08-25 17:02:57 -0300

    feat: Exercicios aula Matheus

---
+++ javascript/atividades/cnh.js
@@ -0,0 +1,17 @@
+//switch
+let cnh = "b"
+switch (cnh) {
+    case "A":
+        case "a":
+        console.log("Pode pilotar moto")
+        break;
+
+    case "B":
+        case "b":
+        console.log("Pode dirigir carro")
+        break;
+
+    default:
+        console.log("Inválido!")
+        break;
+}
---
+++ javascript/atividades/fruit.js
@@ -0,0 +1,17 @@
+let fruit = "Morango"
+
+switch (fruit) {
+    case "Laranja":
+        console.log("8,00R$ O KG")
+        break;
+    case "Morango":
+        console.log("10,00R$ a caixa")
+        break;
+case "Banana":
+console.log("10,00R$ a dúzia")
+break;
+case "Carambola":
+    console.log("15,00R$ o KG")
+    default:
+        break;
+}
